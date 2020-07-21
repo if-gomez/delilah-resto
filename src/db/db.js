03d944d9abc3180fc9f9b60e2b1084f5353048ad
if-gomez <ivangomez1902@gmail.com>
2020-07-21 20:40:15 -0300

    Heroku changes

--- src/db/db.js
+++ src/db/db.js
@@ -2,10 +2,7 @@ const Sequelize = require('sequelize');
 const mysql2 = require('mysql2');
 const {db_dialect, db_host, db_password, db_port, db_username, db_database} = require('../../config');
 
-const db = new Sequelize(`mysql://be719d462a6ffb@us-cdbr-east-02.cleardb.com:${db_port}/heroku_fadb7e7045e315e`);
-
-//mysql://be719d462a6ffb:1355c1c0@us-cdbr-east-02.cleardb.com/heroku_fadb7e7045e315e
-//           user          pass        host                         database
+const db = new Sequelize(`${db_dialect}://${db_username}@${db_host}:${db_port}/${db_database}`);
 
 db.authenticate()
   .then(() => console.log('Base de datos conectada'))
