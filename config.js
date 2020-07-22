3abc41282936f8cea0af66db5f41690d3381c425
if-gomez <ivangomez1902@gmail.com>
2020-07-21 23:10:55 -0300

    Heroku changes

---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {
+    JWT_KEY: 'VB32^rtUMG&U',
+    saltRounds: 10,
+    db_username: 'be719d462a6ffb',
+    db_password:'1355c1c0',
+    db_host: 'us-cdbr-east-02.cleardb.com',
+    db_port: '3306',
+    db_dialect: 'mysql',
+    db_database: 'heroku_fadb7e7045e315e'
+};
+
+//mysql://be719d462a6ffb:1355c1c0@us-cdbr-east-02.cleardb.com/heroku_fadb7e7045e315e
+//           user          pass        host                         database
+
+module.exports = config;
--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,9 @@ const Sequelize = require('sequelize');
 const mysql2 = require('mysql2');
 const {db_dialect, db_host, db_password, db_port, db_username, db_database} = require('../../config');
 
-const db = new Sequelize(`${db_dialect}://${db_username}@${db_host}:${db_port}/${db_database}`);
+//const db = new Sequelize(`${db_dialect}://${db_username}@${db_host}:${db_port}/${db_database}`);
+
+const db = new Sequelize(`${db_dialect}://${db_username}@${db_host}/${db_database}`);
 
 db.authenticate()
   .then(() => console.log('Base de datos conectada'))
